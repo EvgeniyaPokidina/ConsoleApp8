6c54adc01bdb724d729af8348044d0fbc51f627c
ЕвгенияПокидина <ovee9191@gmail.com>
2024-04-03 00:01:24 +0300

    lesson 7#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите значение l:");
+double l = double.Parse(Console.ReadLine()); ;
+Console.WriteLine(l.ToString());
+Console.WriteLine(l.GetType());
+
+
+
+Console.Write("Введите значение x:");
+int x = int.Parse(Console.ReadLine()); ;
+Console.WriteLine(x.ToString());
+Console.WriteLine(x.GetType());
+
