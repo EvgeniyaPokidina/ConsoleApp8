fdbdd325d4cbf3d2f965bb0558ae7475df1bb87e
ЕвгенияПокидина <ovee9191@gmail.com>
2024-04-03 00:05:36 +0300

    ConsoleAPP1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,6 @@ double l = double.Parse(Console.ReadLine()); ;
 Console.WriteLine(l.ToString());
 Console.WriteLine(l.GetType());
 
-
 Console.Write("Введите значение x:");
 int x = int.Parse(Console.ReadLine()); ;
 Console.WriteLine(x.ToString());
