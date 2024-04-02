d0f349155d6bfd11f12984927b50c84930d11222
User0 <User0@DESKTOP-J6BCL92>
2024-04-02 21:02:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,20 @@
+﻿//string number = "28";
+//byte bt = Convert.ToByte(number);
+//Console.WriteLine(bt.GetType());
+//Console.WriteLine(bt);
+
+//nint n = bt;
+//Console.WriteLine(n.GetType());
+//Console.WriteLine(n);
+
+//int integer = Convert.ToInt32(n);
+//Console.WriteLine(integer.GetType());
+//Console.WriteLine(integer);
+
+//long lg = integer;
+//Console.WriteLine(lg.GetType());
+//Console.WriteLine(lg);
+
+//double db = lg;
+//Console.WriteLine(db.GetType());
+//Console.WriteLine(db);
---
+++ Lesson8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.Write("Сколько книг взял Петя:");
+int x = 10;
+Console.WriteLine(x);
+Console.WriteLine("Сколько книг взял Алеша:");
+string a = ($" {x -= 4}");
+Console.WriteLine(x);
+Console.WriteLine("Сколько книг взял Ваня:");
+string b= ($" {x += 2}");
+Console.WriteLine(x);
+string summ = x + a + b;
+Console.WriteLine(summ);
+
+//Console.Write("Введите количество:");
+//int count1 = int.Parse(Console.ReadLine());
+//Console.Write("Введите цену:");
+//decimal price1 = int.Parse(Console.ReadLine());
