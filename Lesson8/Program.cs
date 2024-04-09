060da9d1485949c1c8004e769277bc01be1e29fa
ЕвгенияПокидина <ovee9191@gmail.com>
2024-04-09 14:23:27 +0300

    Lesson8

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -1,18 +1,25 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿//В школьной библиотеке дети получают книги.
+//Петя взял 10 книг, Алеша – на 4 книги меньше, чем Петя, а Ваня
+//на 2 книги больше чем Алеша.
+//Сколько книг взял каждый мальчик?
+//Сколько книг они взяли вместе?
 
 Console.Write("Сколько книг взял Петя:");
-int x = 10;
-Console.WriteLine(x);
-Console.WriteLine("Сколько книг взял Алеша:");
-string a = ($" {x -= 4}");
-Console.WriteLine(x);
-Console.WriteLine("Сколько книг взял Ваня:");
-string b= ($" {x += 2}");
-Console.WriteLine(x);
-string summ = x + a + b;
+int x=int.Parse(Console.ReadLine());
+Console.Write("Сколько книг взял Алеша:");
+int y=(x-4);
+Console.WriteLine(y);
+Console.Write("Сколько книг взял Ваня:");
+int z = (y + 2);
+Console.WriteLine(z);
+int summ = x + y + z;
 Console.WriteLine(summ);
 
-//Console.Write("Введите количество:");
-//int count1 = int.Parse(Console.ReadLine());
-//Console.Write("Введите цену:");
-//decimal price1 = int.Parse(Console.ReadLine());
+//2/ В первой корзине 23 яблока, а во второй на 11 яблок меньше.Сколько яблок в обеих корзинах?
+Console.Write("Сколько яблок в первой корзине:");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Сколько яблок во второй корзине:");
+int x2 = (x1 - 11);
+Console.WriteLine(x2);
+int summa = x1+x2;
+Console.WriteLine(summa);
