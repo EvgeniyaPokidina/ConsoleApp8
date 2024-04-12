0a85f8ccdb9c45a39e5384a81ba4cbbe6434be87
ЕвгенияПокидина <ovee9191@gmail.com>
2024-04-12 09:18:23 +0300

    Lesson 10.1

---
+++ Lesson 10.1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Написать программу, которая запрашивает у пользователя значение радиуса окружности и выводит на экран ее длину и площадь, вычисленные с помощью методов класса Math
+
+
+Console.Write("Введите радиус:");
+const double PI = Math.PI;
+double R = double.Parse(Console.ReadLine());
+double S = PI * R * R;
+double L = 2 * PI * R;
+Console.WriteLine($"Площадь: {S:F2} Длина: {L:F2}");
