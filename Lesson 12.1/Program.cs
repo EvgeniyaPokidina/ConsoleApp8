1255a41f6a2d8ca1431de62b87fe3aaf79830cca
ЕвгенияПокидина <ovee9191@gmail.com>
2024-05-01 17:04:21 +0300

    Lesson 12.1

---
+++ Lesson 12.1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Домашнее задание. Тема 12. Урок 1. Операторы ветвления Switch/ Case.
+//Калькулятор: пользователь может выбрать операцию (сложение,вычитание, умножение, деление), ввести два числа и получить результат
+
+
+Console.Write("Введите 1 число:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число:");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите оператор:");
+char oper;
+oper=Convert.ToChar(Console.ReadLine());
+
+if (oper == '+')
+{
+    Console.WriteLine($"Общая сумма равна {a + b}");
+}
+else if (oper == '-')
+{
+    Console.WriteLine($"Общая сумма равна {a - b}");
+}
+else if (oper == '*')
+{
+    Console.WriteLine($"Общая сумма равна {a * b}");
+}
+else if (oper == '/')
+{
+    Console.WriteLine($"Общая сумма равна {a / b}");
+}
+else
+{
+    Console.WriteLine("Неизвестный оператор");
+}
