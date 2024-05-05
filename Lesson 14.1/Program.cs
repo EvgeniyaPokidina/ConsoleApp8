f890e7aa62cb701218c87c892020e88d3eed6c5e
ЕвгенияПокидина <ovee9191@gmail.com>
2024-05-05 21:10:16 +0300

    Lesson 14.1

--- Lesson 14.1/Program.cs
+++ Lesson 14.1/Program.cs
@@ -6,31 +6,17 @@
 //Для реализации генерации пароля требуется использовать цикл for.
 //Программа должна создавать случайный набор символов, выбирая из списка допустимых символов на каждой итерации цикла.
 
-//Console.Write("Введите пароль:");
-////int n = int.Parse(Console.ReadLine());
-//string str = Console.ReadLine();
-//string a = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//Random random = new Random();
-//string parol = "";
-//for (int i = 1; i <= n; i++)
-//{
-//    int b = random.Next(a.Length);
-//    parol += a[b];
-//}
-//Console.WriteLine("Ваш пароль = " + parol);
-
-
-Console.Write("Введите длину пароля:");
-string str = Console.ReadLine();
-//int n = int.Parse(Console.ReadLine());
-string str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-Random random=new Random();
-for (int i = 0; i < str; i++)
-{ 
-int parol = random.Next(str.Length);
-    Console.WriteLine(parol);
+Console.Write("введите длину пароля ");
+int n = int.Parse(Console.ReadLine());
+string a = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+Random random = new Random();
+string parol = "";
+for (int i = 1; i <= n; i++)
+{
+    int b = random.Next(a.Length);
+    parol += a[b];
 }
+Console.WriteLine("Ваш пароль = " + parol);
 
-//Console.Write("Введите строку:");
-//string str=Console.ReadLine();
-//for(int i=str.Length-1; i>=0; i--) Console.Write(str[i]);
+
+
