6cd6d9e5a20fdfe40fa525e7c513793352387dca
ЕвгенияПокидина <ovee9191@gmail.com>
2024-05-19 19:53:34 +0300

    Les 17.1

--- Les 17.1/Program.cs
+++ Les 17.1/Program.cs
@@ -3,7 +3,7 @@
 //Вывести весь массив на консоль.
 
 int[] mas1 = {1,2,3,4,5};
-int[] mas2 = {6,7,8,9,10 };
+int[] mas2 = {6,7,8,9,10};
 
 for (int i = 0; i < mas1.Length; i++)
 {
@@ -28,15 +28,3 @@ for (int i = 0; i < mas2.Length; i++)
     Console.Write(mas2[i] + " ");
 }
 Console.WriteLine();
-//Console.WriteLine();
-//int[,] mas3 = new int[5, 5];
-//for (int i = 0; i < mas3.GetLength(0); i++)
-//{
-//    for (int j = 0; j < mas3.GetLength(1); j++)
-//    {
-//        if (i == 0) mas3[i, j] = mas1[j];
-//        else mas3[i, j] = mas2[j];
-//        Console.Write(mas3[i, j] + " ");
-//    }
-//    Console.WriteLine();
-//}
