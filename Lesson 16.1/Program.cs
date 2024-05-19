85515fb330ba7ef5b474945c5253ab3f3f3ae19b
ЕвгенияПокидина <ovee9191@gmail.com>
2024-05-19 19:43:41 +0300

    Lesson 16.1

---
+++ Lesson 16.1/Program.cs
@@ -0,0 +1,33 @@
+﻿string[,] mas1 =
+{
+    {"","","x","x","",""},    {"","x","x","x","x",""},
+    {"x","x","","","x","x"},    {"","x","x","x","x",""},
+    {"","","x","x","",""},    {"","","x","x","",""},
+    {"","","x","x","",""},    {"","","x","x","",""},
+    {"","","x","x","",""},    {"","","x","x","",""},
+    {"","x","x","x","x",""}
+};
+string answer = "no"; bool isChecked = false;
+do
+{
+    Console.Clear(); for (int j = 0; j < mas1.GetUpperBound(0) + 1; j++)
+    {
+        for (int l = 0; l < mas1.GetUpperBound(1) + 1; l++)
+        {
+             Console.Write($"{mas1[j, l],2}");
+        }
+        Console.WriteLine();
+    }
+    if (isChecked == false) Console.Write("Включить лампочку?"); else Console.Write("Выключить лампочку?");
+    answer = Console.ReadLine()!; if (answer == "yes") isChecked = !isChecked;
+    if (isChecked)
+    {
+        mas1[2, 2] = "x"; mas1[2, 3] = "x";
+    }
+    else
+    {
+        mas1[2, 2] = "";
+        mas1[2, 3] = "";
+    }
+}
+while (true);
