bacc5a9d1ee9f81a13810d8887759449f201209b
ЕвгенияПокидина <ovee9191@gmail.com>
2024-05-26 22:10:00 +0300

    Lesson 18.2

---
+++ 18.1/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] words = { "СВЕТ", "ЦВЕТОК", "СОЛНЦЕ", "ВОДА", "ДЕНЬ" };
+string sentence = "";
+string word = "";
+do
+
+{
+    Console.Write("Введите слово:");
+    word = Console.ReadLine();
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (word == words[i])
+        {
+            word = "(скрыто)";
+
+            break;
+        }
+    }
+    sentence += word + " ";
+}
+while (word != "конец");
+Console.WriteLine(sentence);
+
+
+
+
+
---
+++ Lesson 18.2/Program.cs
@@ -0,0 +1,19 @@
+﻿//1
+Console.Write("Введите строку:");
+string str = Console.ReadLine();
+string[] mas = str.Split(' ');
+int sum = 0;
+int[] array = new int[str.Length];
+for (int i = 0; i < mas.Length; i++)
+{
+    int a = int.Parse(mas[i]);
+    array[i] = int.Parse(mas[i]);
+    sum += a;
+}
+Console.WriteLine("Сумма = " + sum);
+
+//2 задача
+//Console.Write("Введите предложение:");
+//string sentense = Console.ReadLine()!;
+//string[] mas1 = sentense.Split(" ");
+//Console.WriteLine("Количество слов в предложении = " + mas1.Length);
