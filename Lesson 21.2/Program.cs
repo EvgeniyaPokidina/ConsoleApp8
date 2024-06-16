52dbed098d07d1e094d730e4455dd8f8b3a9bd9a
ЕвгенияПокидина <ovee9191@gmail.com>
2024-06-16 12:38:22 +0300

    Lesson 21.2

---
+++ Lesson 21.2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая просит пользователя ввести путь к файлу на компьютере. Затем программа должна проверить, существует ли указанный файл,
+//и вывести соответствующее сообщение. Если файл существует, программа должна также вывести его размер в байтах, дату последнего изменения и расширение файла
+
+using System.ComponentModel.Design;
+
+Console.Write("ВВедите путь к файлу:");
+string path = Console.ReadLine()!;
+if (File.Exists(path)) 
+{
+    Console.WriteLine("Файл существует");
+    FileInfo fileInfo = new FileInfo(path);
+    Console.WriteLine("Размер файла в байтах:" +fileInfo.Length);
+    Console.WriteLine("Дата последнего изменения:" + fileInfo.LastWriteTime);
+    Console.WriteLine("Расширение файла:" + fileInfo.Extension);
+}
+ else
+{ 
+    Console.WriteLine("Файл не существует"); 
+}
