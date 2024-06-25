581031a2f36bf4201a4573e87d770e3aff7e4f37
ЕвгенияПокидина <ovee9191@gmail.com>
2024-06-25 15:41:16 +0300

    Lesson 22.2

--- Lesson 22.2/Program.cs
+++ Lesson 22.2/Program.cs
@@ -1,28 +1,18 @@
 ﻿//Напишите программу, которая запрашивает у пользователя список чисел, разделенных пробелами, и вычисляет их сумму. Обработайте исключение,
 //которое может возникнуть при некорректном вводе (например, если введены символы, отличные от цифр и пробелов).
 
-
 try
 {
     Console.Write("Введите первое число:");
-    int n = int.Parse(Console.ReadLine()!);
+    int x = int.Parse(Console.ReadLine()!);
     Console.Write("Введите второе число:");
-    int m = int.Parse(Console.ReadLine()!);
-    Console.WriteLine(n+m);
+    int y = int.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Сумма равна {x + y}");
 }
 catch (FormatException e)
 {
     Console.WriteLine(e.Message);
 }
 
-//try
-//{
-//    Console.Write("Введите положительное число:");
-//    int n = int.Parse(Console.ReadLine()!);
-//    if (n < 0) throw new Exception("Корень от отрицательного числа!!!");
-//    Console.WriteLine(Math.Sqrt(n));
-//}
-//catch(Exception e)
-//{
-//    Console.WriteLine(e.Message);
-//}
+
+
