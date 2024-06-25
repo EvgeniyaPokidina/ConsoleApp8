c74c3facef751e4fa56a5b98311bacc33ff062c5
ЕвгенияПокидина <ovee9191@gmail.com>
2024-06-25 23:50:00 +0300

    Lesson 22.1

---
+++ Lesson 22.1/Program.cs
@@ -0,0 +1,53 @@
+﻿//Ошибка при делении на 0
+//double Division(double a, double b)
+//{
+//    if (b != 0) return a / b;
+
+//    throw new DivideByZeroException("Делить на ноль нельзя!");
+//}
+//    try
+//    {
+//        Console.Write("Введите первое число:");
+//        int n = int.Parse(Console.ReadLine()!);
+//        Console.Write("Введите второе число:");
+//        int m = int.Parse(Console.ReadLine()!);
+//        Console.WriteLine(Division(n, m));
+//    }
+//    catch(DivideByZeroException e)
+//    {
+//        Console.WriteLine (e.Message);
+//    }
+//выход за пределы массива
+//try
+//{
+//    int[] mas = new int[5];
+//    Console.WriteLine(mas[5]);
+//    Random random = new Random();
+//}
+//catch(IndexOutOfRangeException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+
+//ошибка формата
+//try
+//{
+//    Console.Write("Введите число:");
+//    int x = int.Parse(Console.ReadLine()!);
+//}
+//catch (FormatException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+//
+try
+{
+    Console.Write("Введите число:");
+    int n = int.Parse(Console.ReadLine()!);
+    if (n < 0) throw new Exception("Корень от отрицательного числа!!!");
+    Console.WriteLine(Math.Sqrt(n));
+}
+catch (ArithmeticException e)
+{
+    Console.WriteLine(e.Message);
+}
