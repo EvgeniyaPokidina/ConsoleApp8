ddd7d6559183316311f0fd3cc2d68b0af0b7a857
ЕвгенияПокидина <ovee9191@gmail.com>
2024-07-02 20:03:34 +0300

    Lesson 24.1

---
+++ Lesson 24.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Дано слово, состоящее только из строчных букв. Проверьте, является ли это слово палиндромом.
+//Выведите Is a palindrom если слово таковым является и Not a palindrom в
+//противном случае
+Console.Write("Введите слово:");
+string word = Console.ReadLine()!;
+Console.WriteLine(IsPalindrome(word));
+static bool IsPalindrome(string word)
+{
+    if (word.Length <= 1)
+    {
+        Console.WriteLine("Is a palindrom");
+        return true;
+    }
+    else
+    {
+        if (word[0] != word[word.Length - 1])
+        {
+            Console.WriteLine("Not a palindrom");
+            return false;
+        }
+        else
+        {
+            return IsPalindrome(word.Substring(1, word.Length - 2));
+        }
+    }
+}
+
