1c4a1080ac77068611d2b1fb02f92dac0e1a4987
ЕвгенияПокидина <ovee9191@gmail.com>
2024-08-05 20:09:49 +0300

    Lesson 27.1

--- Lesson 27.1/Program.cs
+++ Lesson 27.1/Program.cs
@@ -1,9 +1,23 @@
 ﻿using Lesson_27._1;
-
+//private: Методы или члены с модификатором private доступны только в пределах того же класса, в котором они объявлены.
+//Они не доступны из других классов. private обеспечивает наивысший уровень защиты данных и инкапсуляцию.
+//public: Методы или члены с модификатором public доступны из любой части программы, даже из других классов.
+//protected: Методы или члены с модификатором protected доступны внутри того же класса, также в производных классах.
+//Они не доступны из других классов, не являющихся производными. 
 static void Main(string[] args)
 {
-    Console.Write("Укажите количество рублей:");
+    Console.Write("Укажите количество рублей: ");
     decimal rub;
     decimal.TryParse(Console.ReadLine(), out rub);
-
+    decimal rubToDollars=RubleConverter.toDollars(rub);
+    Console.WriteLine($"{rubToDollars:F2} долларов");
+    decimal rubToEuros = RubleConverter.ToEuros(rub);
+    Console.WriteLine($"{rubToEuros:F2} евро");
+    decimal rubToZlotys = RubleConverter.ToZlotys(rub);
+    Console.WriteLine($"{rubToZlotys:F2} злотых");
+    decimal rubToYuan = RubleConverter.ToYuan(rub);
+    Console.WriteLine($"{rubToYuan:F2} юаней");
+    decimal rubToRupes = RubleConverter.ToRupes(rub);
+    Console.WriteLine($"{rubToRupes:F2} рупий"); 
 }
+Main(args);
--- Lesson 27.1/RubleConverter.cs
+++ Lesson 27.1/RubleConverter.cs
@@ -10,7 +10,10 @@ namespace Lesson_27._1
  
     public static class RubleConverter
     {
-        public static decimal toDollars(decimal rub)=>  rub * 88.61m;
-        
+        public static decimal toDollars(decimal rub)=>  rub / 84.95m;
+        public static decimal ToEuros(decimal rub) => rub / 93.04m;
+        public static decimal ToZlotys(decimal rub) => rub / 21.6m;
+        public static decimal ToYuan(decimal rub) => rub / 11.83m;
+        public static decimal ToRupes(decimal rub) => rub / 1.01m;
     }
 }
