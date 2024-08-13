9f2968fdcc607650cc736a9a97604da6ab5ff107
ЕвгенияПокидина <ovee9191@gmail.com>
2024-08-13 19:57:28 +0300

    Lesson 27.4

---
+++ Lesson 27.4/Numbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_27._4
+{
+    internal class Numbers
+    {
+        public double CalculateAverage(int[] numbers)
+        {
+            double sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum / numbers.Length;
+        }
+        public bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+        public bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+        public long CalculateFactorial(int number)
+        {
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+       public string ConvertToBinary(int value) 
+        {
+           return Convert.ToString(value,2);
+        }
+        public string ConvertToHex(int number)
+        {
+            return Convert.ToString(number, 16);
+        }
+    
+    }
+}
+
---
+++ Lesson 27.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Lesson_27._4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Numbers numbers = new Numbers();
+        int[] array = { 10, 27, 31, 4, 15 };
+        double average = numbers.CalculateAverage(array);
+        Console.WriteLine($"Среднее арифметическое: {average}");
+        Console.WriteLine("Введите число для проверки");
+        int x;
+        int.TryParse(Console.ReadLine(), out x);
+        Console.WriteLine($"Число является простым? {numbers.IsPrime(x)}");
+        int y;
+        int.TryParse(Console.ReadLine(), out y);
+        Console.WriteLine($"Число является четным? {numbers.IsEven(y)}");
+        int z;
+        int.TryParse(Console.ReadLine(), out z);
+        Console.WriteLine($"Число является не четным? {numbers.IsOdd(z)}");
+        Console.Write("Введите число для вычисления факториала:");
+        int fact;
+        int.TryParse(Console.ReadLine(), out fact);
+        long factorial = numbers.CalculateFactorial(fact);
+        Console.WriteLine($"Факториал числа {fact} равен {factorial}");
+        Console.Write("Введите число для преобразования в двоичное представление:");
+        int binary;
+        int.TryParse(Console.ReadLine(), out binary);
+        string BinaryRepresent = numbers.ConvertToBinary(binary);
+        Console.WriteLine($"Число {binary} в двоичном представлении {BinaryRepresent}");
+        Console.Write("Введите число для преобразования в шестнадцатиричном представление:");
+        int numberHex;
+        int.TryParse(Console.ReadLine(), out numberHex);
+        string hexRepresent= numbers.ConvertToHex(numberHex);
+        Console.WriteLine($"Число {numberHex} в шестнадцатиричной представлении {hexRepresent}");
+    }
+}
+
